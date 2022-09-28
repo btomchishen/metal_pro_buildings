596ad7b8aab5f97df564d262f2535a80bfec6eec
btomchishen <btomchishen@icloud.com>
2022-09-28 15:42:36 +0300

    Tasks Updater v2.0

--- task_updater/index.php
+++ task_updater/index.php
@@ -2,26 +2,35 @@
 define('STOP_STATISTICS', true);
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 
-
 CJSCore::Init(array("jquery", "ajax"));
 $APPLICATION->SetTitle('Tasks updater');
 ?>
-    <div id="page"></div>
-    <button id="btn">Update</button>
-    <div id="result"></div>
+    <div id="info"></div>
+    <button id="updateButton">Update</button>
+    <table id="resultTable">
+        <tr>
+            <td>Page Number</td>
+            <td>IDs Range</td>
+            <td>Status</td>
+            <td>Time processing</td>
+        </tr>
+    </table>
+    </div>
+
     <script>
+        const itemsPerPage = 100;
+        const ajaxUrl = '/task_updater/task_update.php';
+
         let tasksCount = 0;
-        let highestId = 0;
         let lastId = 0;
         let page = 1;
         let pagesCount = 0;
         let operatedPages = 0;
-        let itemsPerPage = 100;
 
-        window.addEventListener('load', (event) => {
+        $(document).ready(function () {
             $.ajax({
                 type: 'POST',
-                url: '/task_updater/task_update.php',
+                url: ajaxUrl,
                 data: {
                     "action": "getTasksCount",
                 },
@@ -32,87 +41,68 @@ $APPLICATION->SetTitle('Tasks updater');
 
                     pagesCount = calculatePagesCount(tasksCount)
 
-                    let pages = document.getElementById('page');
-                    pages.innerHTML = 'Operated pages: ' + '0/' + pagesCount
-
-                    BX.UI.Notification.Center.notify({
-                        content: "Tasks count: " + parsedData.tasksCount,
-                        position: "top-right"
-                    });
-                },
-            })
-
-
-            $.ajax({
-                type: 'POST',
-                url: '/task_updater/task_update.php',
-                data: {
-                    "action": "getHighestId",
-                },
-                async: false,
-                success: function (data) {
-                    let parsedData = JSON.parse(data)
-                    highestId = parsedData.highestId;
-
-                    BX.UI.Notification.Center.notify({
-                        content: "Highest Id: " + parsedData.highestId,
-                        position: "top-right"
-                    });
-                },
-            })
-
-            document.getElementById('btn').addEventListener('click', (event) => {
-                let res = document.getElementById('result');
-                let pages = document.getElementById('page');
-                pagesCount = 3
-                while (page <= pagesCount) {
-                    var x = test()
-                    console.log(x)
-
-                    res.innerHTML += 'Page: ' + x.page + ', IDs: ' + lastId + '-' + x.lastId + ' - ' + status + '<br>'
-                    pages.innerText = 'Operated pages: ' + operatedPages + '/' + pagesCount
-                    page++;
+                    $('#info').html('Tasks count: ' + tasksCount + '<br>Operated pages: ' + '0/' + pagesCount)
                 }
+            })
 
+            $('#updateButton').on('click', function (event) {
+                updateTasks();
             })
         })
-
-        function test() {
-            var res = $.ajax({
-                type: 'POST',
-                url: '/task_updater/task_update.php',
-                data: {
-                    "action": "test",
-                    "page": page,
-                    "itemsPerPage": itemsPerPage
-                },
-                async: false,
-                success: function (data) {
-                    let parsedData = JSON.parse(data)
-                    operatedPages++
-
-                    let status = '';
-                    switch (parsedData.status) {
-                        case "Success":
-                            status = '<span style="color: green;">' + parsedData.status + '</span>'
-                            break;
-                        default:
-                            status = '<span style="color: red;">' + parsedData.status + '</span>'
-                            break;
-                    }
-
-                    // res.innerHTML += 'Page: ' + parsedData.page + ', IDs: ' + lastId + '-' + parsedData.lastId + ' - ' + status + '<br>'
-                    // pages.innerText = 'Operated pages: ' + operatedPages + '/' + pagesCount
-                    lastId = parsedData.lastId
-                },
-            }).responseText
-
-            return JSON.parse(res);
-        }
 
         function calculatePagesCount(tasksCount) {
             return Math.ceil(tasksCount / itemsPerPage);
         }
+
+        function formatStatus(status) {
+            if (status === 'Success')
+                return '<span style="color: green;">' + status + '</span>'
+            else
+                return '<span style="color: red;">' + status + '</span>'
+        }
+
+        function prepareRowForTable(page, fromId, toId, status, time) {
+            return '<tr>' +
+                '<td>' + page + '</td>' +
+                '<td>' + fromId + '-' + toId + '</td>' +
+                '<td>' + status + '</td>' +
+                '<td>' + time + '</td>' +
+                '</tr>'
+        }
+
+        function updateTasks() {
+            $.ajax({
+                type: 'POST',
+                url: ajaxUrl,
+                data: {
+                    "action": "tasksUpdate",
+                    "page": page,
+                    "itemsPerPage": itemsPerPage
+                },
+                async: true,
+                success: function (data) {
+                    let parsedData = JSON.parse(data)
+                    operatedPages++
+
+                    $('#resultTable').append(prepareRowForTable(parsedData.page, lastId, parsedData.lastId, formatStatus(parsedData.status), parsedData.time))
+                    $('#info').html('Tasks count: ' + tasksCount + '<br>Operated pages: ' + operatedPages + '/' + pagesCount)
+
+                    lastId = parsedData.lastId
+                    if (page <= pagesCount) {
+                        updateTasks();
+                    }
+                },
+            })
+            page++
+        }
     </script>
+
+    <style>
+        #resultTable {
+            width: 40%;
+            text-align: center;
+            border: 1px solid;
+        }
+    </style>
 
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
